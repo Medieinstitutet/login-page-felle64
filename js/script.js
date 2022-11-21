2aabc8a460c96556c69bdac28de01a8cf9d6a837
felle64 <felle641@gmail.com>
2022-11-21 13:46:07 +0100

    first commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-console.log("Hello world");
+let uName = document.getElementById("uname");
+let psw = document.getElementById("psw");
+let loginBtn = document.getElementById("loginBtn");
+
+console.log(uName.value);
+console.log(psw.value);
