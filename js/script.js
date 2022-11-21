0f24fd3d3d55f7c7bcd98e25db302e76a2c278ff
felle64 <felle641@gmail.com>
2022-11-21 18:58:47 +0100

    minor improvements

--- js/script.js
+++ js/script.js
@@ -1,6 +1,33 @@
-let uName = document.getElementById("uname");
-let psw = document.getElementById("psw");
+//Hämtar elementen.
+let inputName = document.getElementById("inputName");
+let inputPsw = document.getElementById("inputPsw");
 let loginBtn = document.getElementById("loginBtn");
 
-console.log(uName.value);
-console.log(psw.value);
+let uNamePswList = [
+    {userName: "janne", passWord: "test"},
+    {userName: "Felix", passWord: "admin"}
+];
+
+function loginError(){
+    alert("Wrong Username and/or password")
+} 
+
+//Sätter så den ska läsa av value i id.
+    let userName = inputName.value;
+    let passWord = inputPsw.value;
+    localStorage.setItem("userName", userName);
+//Loggar lite
+console.log(userName);
+console.log(passWord);
+
+loginBtn.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    
+    if (userName === "admin" && passWord === "admin") {
+        alert("You have successfully logged in.");
+    
+    } else {
+        loginError()
+       
+    }
+})
