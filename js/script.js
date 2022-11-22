c8e477899853da8f0ffdeb2a0950893f596593d5
felle64 <felle641@gmail.com>
2022-11-22 13:40:56 +0100

    improvements

--- js/script.js
+++ js/script.js
@@ -3,31 +3,37 @@ let inputName = document.getElementById("inputName");
 let inputPsw = document.getElementById("inputPsw");
 let loginBtn = document.getElementById("loginBtn");
 
-let uNamePswList = [
+/*let uNamePswList = [
     {userName: "janne", passWord: "test"},
     {userName: "Felix", passWord: "admin"}
 ];
+console.log(uNamePswList);*/
 
-function loginError(){
+//Felmedelande för fel inloggningsuppgifter
+function printLoginError(){
     alert("Wrong Username and/or password")
 } 
+//Visar att man har loggat in och att man är inloggad
+function printLoginSuccess(){
+    alert("You have successfully logged in.")
+}
 
-//Sätter så den ska läsa av value i id.
+loginBtn.addEventListener("click", (evt) => {
+    //Fånga usrname och psw i LS
+    evt.preventDefault();
+
     let userName = inputName.value;
     let passWord = inputPsw.value;
     localStorage.setItem("userName", userName);
-//Loggar lite
-console.log(userName);
-console.log(passWord);
-
-loginBtn.addEventListener("click", (evt) => {
-    evt.preventDefault();
+    localStorage.setItem("passWord", passWord);
+    console.log(userName);
+    console.log(passWord);
     
     if (userName === "admin" && passWord === "admin") {
-        alert("You have successfully logged in.");
+       printLoginSuccess();
     
     } else {
-        loginError()
+        printLoginError();
        
     }
 })
