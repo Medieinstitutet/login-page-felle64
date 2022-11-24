c0430edabd2dc040e8a63fa299f4fb538acb29b6
felle64 <felle641@gmail.com>
2022-11-24 12:53:26 +0100

    major progress

--- js/script.js
+++ js/script.js
@@ -2,7 +2,18 @@
 const inputName = document.getElementById("inputName");
 const inputPsw = document.getElementById("inputPsw");
 const loginBtn = document.getElementById("loginBtn");
+const logoutBtn = document.getElementById("logoutBtn");
+const loginErrorMsg = document.getElementsByClassName("loginErrorMsg");
+const labelUname = document.getElementById("labelUname");
+const labelPass = document.getElementById("labelPass");
 h1 = document.querySelector("h1")
+
+logoutBtn.style.display = "none";
+loginErrorMsg[0].style.display = "none";
+
+let userName = inputName.value;
+let passWord = inputPsw.value;
+
 
 let user = [
     {userName: "janne", passWord: "test"},
@@ -10,24 +21,37 @@ let user = [
 ];
 
 
+//Gör ett logintoken som gör att man ska stanna inloggad
+function loginStatus(){
+    if (localStorage.getItem("userName")){
+        h1.innerHTML = "Welcome " + localStorage.getItem("userName");
+        logoutBtn.style.display = "block"
+        inputName.style.display = "none"
+        inputPsw.style.display = "none"
+        labelUname.style.display = "none"
+        labelPass.style.display = "none"
+        loginBtn.style.display = "none"
+        
+    }else{
+        h1.innerHTML ="Login Page";
+    }
+}
 
+loginStatus()
 
 //Felmedelande för fel inloggningsuppgifter temp
 function printLoginError(){
-    alert("Wrong Username and/or password")
+    //alert("Wrong Username and/or password")
+    loginErrorMsg[0].style.display = "block";
 } 
 //Visar att man har loggat in och att man är inloggad temp
 function printLoginSuccess(){
-    alert("You have successfully logged in.");
-    let userName = inputName.value;
-    let passWord = inputPsw.value;
+    //alert("You have successfully logged in.");
+   
     localStorage.setItem("userName", userName);
     localStorage.setItem("passWord", passWord);
-    h1.innerHTML = "Welcome " + localStorage.getItem("userName");
-    
-    localStorage.getItem("passWord", passWord);
-    
-    //location.reload();
+    //h1.innerHTML = "Welcome " + localStorage.getItem("userName");
+    location.reload();
 }
 
 loginBtn.addEventListener("click", (evt) => {
@@ -48,12 +72,7 @@ loginBtn.addEventListener("click", (evt) => {
     }
 })
 
-    //evt.preventDefault();
-    //Fånga usrname och psw i LS
-    //let userName = inputName.value;
-    //let passWord = inputPsw.value;
-    //localStorage.setItem("userName", userName);
-    //localStorage.setItem("passWord", passWord);
-   // console.log(userName);
-   // console.log(passWord);
-   
+logoutBtn.addEventListener("click", (evt) => {
+    localStorage.clear()
+    location.reload()
+})
