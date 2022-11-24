6067e081df1fb03b7e68d912809f09c6c4f83de9
felle64 <felle641@gmail.com>
2022-11-24 01:23:56 +0100

    progress

--- js/script.js
+++ js/script.js
@@ -2,11 +2,15 @@
 const inputName = document.getElementById("inputName");
 const inputPsw = document.getElementById("inputPsw");
 const loginBtn = document.getElementById("loginBtn");
+h1 = document.querySelector("h1")
 
 let user = [
     {userName: "janne", passWord: "test"},
     {userName: "felix", passWord: "123"}
 ];
+
+
+
 
 //Felmedelande för fel inloggningsuppgifter temp
 function printLoginError(){
@@ -14,7 +18,15 @@ function printLoginError(){
 } 
 //Visar att man har loggat in och att man är inloggad temp
 function printLoginSuccess(){
-    alert("You have successfully logged in.")
+    alert("You have successfully logged in.");
+    let userName = inputName.value;
+    let passWord = inputPsw.value;
+    localStorage.setItem("userName", userName);
+    localStorage.setItem("passWord", passWord);
+    h1.innerHTML = "Welcome " + localStorage.getItem("userName");
+    
+    localStorage.getItem("passWord", passWord);
+    
     //location.reload();
 }
 
@@ -23,17 +35,14 @@ loginBtn.addEventListener("click", (evt) => {
     //console.log("klick");
     let userName = inputName.value;
     let passWord = inputPsw.value;
-    localStorage.getItem("userName", userName);
-    localStorage.getItem("passWord", passWord);
-    localStorage.setItem("userName", userName);
-    localStorage.setItem("passWord", passWord);
-
+    
     for (let i=0; i < user.length; i++) {
         if (user[i].passWord === passWord && user[i].userName === userName)
         return printLoginSuccess();
               
         else{
              printLoginError();
+             
         }
     
     }
